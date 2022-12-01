111c825c77aa44fb51816f68a2cf90f70617a94f
Ezh+Wine <valvic90@gmail.com>
2022-12-01 22:27:32 +0300

    corrections

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 // Задание1
 
-// const numbers = {
-//     keyin1: 1,
-//     keyin2: 2,
-//     keyin3: 3,
-//     keyin4: 4,
-//     keyin5: 5,
-//     keyin6: 6,
-//     keyin7: 7,
-// }
+const numbers = {
+    keyin1: 1,
+    keyin2: 2,
+    keyin3: 3,
+    keyin4: 4,
+    keyin5: 5,
+    keyin6: 6,
+    keyin7: 7,
+}
 
-// for (const key in numbers) {
-//     if (numbers[key] >= 3) {
-//         console.log(numbers[key]);
-//     }
-// }
+for (const key in numbers) {
+    if (numbers[key] >= 3) {
+        console.log(numbers[key]);
+    }
+}
 
 // Или
 
@@ -82,35 +82,37 @@
 
 // console.table(products);
 
+
 // Задание 4
 
-const products = [
-    {
-        id: 3,
-        price: 127,
-        photos: [
-            "1.jpg",
-            "2.jpg",
-        ],
-    },
-    {
-        id: 5,
-        price: 499,
-        photos: [],
-    },
-    {
-        id: 10,
-        price: 26,
-        photos: [
-            "3.jpg",
-        ],
-    },
-    {
-        id: 8,
-        price: 78,
-    },
-];
+// const products = [
+//     {
+//         id: 3,
+//         price: 127,
+//         photos: [
+//             "1.jpg",
+//             "2.jpg",
+//         ],
+//     },
+//     {
+//         id: 5,
+//         price: 499,
+//         photos: [],
+//     },
+//     {
+//         id: 10,
+//         price: 26,
+//         photos: [
+//             "3.jpg",
+//         ],
+//     },
+//     {
+//         id: 8,
+//         price: 78,
+//     },
+// ];
 
+// 4.1
 
 // const productsWithPics = products.filter(item => item.photos && item.photos.length > 0);
 
@@ -132,38 +134,40 @@ const products = [
 
 // console.table(productsWithPics);
 
+// 4.2
 
-products.sort(function (item1, item2) {
-    if (item1.price > item2.price) {
-        return 1;
-    }
-    else if (item1.price < item2.price) {
-        return -1;
-    } else {
-        return 0;
-    }
-}
-);
+// products.sort(function (item1, item2) {
+//     if (item1.price > item2.price) {
+//         return 1;
+//     }
+//     else if (item1.price < item2.price) {
+//         return -1;
+//     } else {
+//         return 0;
+//     }
+// }
+// );
 
-console.table(products);
+// console.table(products);
+
+
 
 // Задание 5
-
 
 // const en = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 // const ru = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"];
 
 
-// function createDayList(arrayEn, arrayRu) {
+// function createDaysList(arrayEn, arrayRu) {
 //     if (arrayEn.length !== arrayRu.length) {
 //         alert('Разная длина массивов')
 //     } else {
 //         const weekDays = {};
-//         for (let i = 0; i < en.length; i++) {
-//             weekDays[en[i]] = ru[i];
+//         for (let i = 0; i < arrayEn.length; i++) {
+//             weekDays[arrayEn[i]] = arrayRu[i];
 //         }
 //         return weekDays;
 //     }
 // }
 
-// console.table(createDayList(en, ru));
+// console.table(createDaysList(en, ru));
